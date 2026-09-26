b27340adb088ceedd3e8bf70719acd3d7b1eca1a
YATHARTH VIKRAM SINGH <tr413512@gmail.com>
2026-09-26 10:42:28 +0530

    feat: upgrade CLF Multi-CEL to sensor fusion

--- data/dp_factory.py
+++ data/dp_factory.py
@@ -1,5 +1,6 @@
 from data.gia_vslam_data_connector import GiaVSLAMdataConnector
 from data.mcel_data_provider import MCELdataProvider
+from data.clf_sensor_fusion_data_provider import CLFSensorFusionDataProvider
 from data.reg_data_provider import RegDataProvider
 from data.tampere_data_connector import TampereDataConnector
 from data.uji_data_connector import UJIdataConnector
@@ -19,41 +20,56 @@ def get_data_provider(dataset_params, m_type):
     elif dataset == 'uts':
         conn = UTSdataConnector()
     elif dataset == 'gia_vslam':
-        conn = GiaVSLAMdataConnector(floors=d_params['floors'],
-                                     devices=d_params['devices'],
-                                     test_devices=d_params['test_devices'] if 'test_devices' in d_params else None,
-                                     test_trajectories=d_params['test_trajectories'] if 'test_trajectories' in d_params else None
-                                     ).load_dataset()
+        conn = GiaVSLAMdataConnector(
+            floors=d_params['floors'],
+            devices=d_params['devices'],
+            test_devices=d_params['test_devices']
+            if 'test_devices' in d_params else None,
+            test_trajectories=d_params['test_trajectories']
+            if 'test_trajectories' in d_params else None
+        ).load_dataset()
     elif dataset == 'clf':
         conn = CLFDataConnector(
             csv_path=d_params.get('csv_path', 'datasets/clf/fingerprints.csv'),
             wifi_prefix=d_params.get('wifi_prefix', 'wifi_'),
             ble_prefix=d_params.get('ble_prefix', 'ble_'),
+            imu_prefix=d_params.get('imu_prefix', 'imu_'),
             motion_features=d_params.get('motion_features'),
             missing_rssi=d_params.get('missing_rssi', -110.0),
         )
     else:
         raise ValueError("Unknown dataset: {}".format(dataset))
 
-    if m_type == 'mCEL':
-        dp = MCELdataProvider(dataset_params['params'], dc=conn).load_dataset().generate_split_indices().generate_validation_indices()
-        dp = dp.replace_missing_values()
+    if m_type in ('mCEL', 'mCEL_sensor_fusion'):
+        if m_type == 'mCEL_sensor_fusion':
+            if dataset != 'clf':
+                raise ValueError(
+                    'mCEL_sensor_fusion currently requires dataset: clf'
+                )
+            dp = CLFSensorFusionDataProvider(
+                dataset_params['params'], dc=conn
+            )
+        else:
+            dp = MCELdataProvider(dataset_params['params'], dc=conn)
 
-        # CLF has mixed feature units. Scale RSSI to approximately [0, 1] while
-        # preserving cyclic heading and normalizing motion separately.
-        if dataset == 'clf':
+        dp = (
+            dp.load_dataset()
+            .generate_split_indices()
+            .generate_validation_indices()
+        )
+
+        if dataset != 'clf':
+            dp = dp.replace_missing_values()
+
+        if m_type == 'mCEL_sensor_fusion':
+            dp = dp.build_sensor_inputs()
+        elif dataset == 'clf':
             x = dp.rss.astype('float32').copy()
             groups = conn.feature_groups
             radio_idx = groups['wifi'] + groups['ble']
             if radio_idx:
                 x[:, radio_idx] = (x[:, radio_idx] + 110.0) / 110.0
                 x[:, radio_idx] = x[:, radio_idx].clip(0.0, 1.0)
-            motion_names = [conn.feature_names[i] for i in groups['motion']]
-            for i, name in zip(groups['motion'], motion_names):
-                if name in ('heading_sin', 'heading_cos'):
-                    continue
-                scale = max(float(abs(x[:, i]).max()), 1.0)
-                x[:, i] /= scale
             dp.x = x
         else:
             dp = dp.standardize_data(scaling_type=d_params['scaling'])
@@ -62,17 +78,27 @@ def get_data_provider(dataset_params, m_type):
 
         class_pad = bool(d_params.get('class_pad', False))
         reg_pad = bool(d_params.get('reg_pad', False))
-        weighted_grid_labels = bool(d_params.get('weighted_grid_labels', False))
+        weighted_grid_labels = bool(
+            d_params.get('weighted_grid_labels', False)
+        )
 
         dp = dp.compute_multilabel_aug_data(weighted_grid_labels)
         dp = dp.set_labels(class_pad, reg_pad)
 
     elif m_type == '3D':
-        dp = RegDataProvider(dataset_params['params'], dc=conn).load_dataset().generate_split_indices().generate_validation_indices()
-        dp = dp.replace_missing_values().standardize_data(scaling_type=d_params['scaling'])
+        dp = (
+            RegDataProvider(dataset_params['params'], dc=conn)
+            .load_dataset()
+            .generate_split_indices()
+            .generate_validation_indices()
+        )
+        dp = (
+            dp.replace_missing_values()
+            .standardize_data(scaling_type=d_params['scaling'])
+        )
         dp = dp.set_labels(scale_labels=True)
 
     else:
-        dp = None
+        raise ValueError('Unsupported model type: {}'.format(m_type))
 
     return dp
