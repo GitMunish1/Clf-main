d50738a80e041707d588fc352542854fabbe42cd
YATHARTH VIKRAM SINGH <tr413512@gmail.com>
2026-09-26 10:43:24 +0530

    feat: complete CLF sensor fusion architecture

--- data/base_data_provider.py
+++ data/base_data_provider.py
@@ -11,45 +11,31 @@ from utils.param_reader import ParamReader
 class BaseDataProvider:
 
     def __init__(self, params, dc: DatasetConnector):
-        # used for consistent train/test split
         self.seed = 1
-
-        # reference to dataset connector
         self.dc = dc
 
-        # unprocessed variables of dataset (will be accessed indirectly via dataset connector)
         self.rss = None
         self.pos = None
         self.floor = None
         self.time = None
         self.num_floors = None
 
-        # list of floorplan dimensions per floor
         self.floorplan_width = None
         self.floorplan_height = None
 
-        # data and labels that are used by model for training
-        # the goal of a specific data provider implementation is to set these two variables
-        # such that a call to model.fit(x, y) succeeds where x and y are obtained via the data provider
         self.x = None
         self.y = None
 
-        # split indices are used for obtaining the required partition (train/val/test) of the dataset
-        # [{'train': [], 'val': [], 'test': [])]
         self._split_indices = None
-
-        # currently, only one train/test split (fold) is supported
         self.num_folds = 1
         self.split_idx = 0
 
-        # reference to parameter reader that falls back to default value if parameter is not specified
         self.pr: ParamReader = ParamReader(params)
 
     def load_dataset(self):
         self.dc.load_dataset()
         return self
 
-    # getters & setters
     @property
     def rss(self):
         return self.dc.rss
@@ -70,8 +56,7 @@ class BaseDataProvider:
     def time(self):
         if hasattr(self.dc, 'time'):
             return self.dc.time
-        else:
-            return None
+        return None
 
     @property
     def num_floors(self):
@@ -85,7 +70,6 @@ class BaseDataProvider:
     def floorplan_height(self):
         return self.dc.floorplan_height
 
-    # getters & setters
     @rss.setter
     def rss(self, val):
         self.dc.rss = val
@@ -129,10 +113,6 @@ class BaseDataProvider:
     def split_indices(self, val):
         self._split_indices = val
 
-    #
-    # Access of data via split indices
-    #
-
     def get_x(self, partition='train'):
         subset = self.split_indices[self.split_idx][partition]
         return self.x[subset]
@@ -144,10 +124,6 @@ class BaseDataProvider:
     def get_data(self, data, partition='train'):
         subset = self.split_indices[self.split_idx][partition]
         return data[subset]
-
-    #
-    # Preprocessing methods
-    #
 
     def replace_missing_values(self, mis_val=100, replace_val=-110):
         self.rss[self.rss == mis_val] = replace_val
@@ -162,19 +138,23 @@ class BaseDataProvider:
         elif scaling_type == 'min_max':
             min_ap_val = np.min(self.rss)
             max_ap_val = np.max(self.rss)
-            self.x = (self.rss - min_ap_val) / (max_ap_val - min_ap_val)
+            self.x = (self.rss - min_ap_val) / (
+                max_ap_val - min_ap_val
+            )
         else:
-            raise NotImplementedError('Specified scaling type: {} is not implemented'.format(scaling_type))
-
+            raise NotImplementedError(
+                'Specified scaling type: {} is not implemented'.format(
+                    scaling_type
+                )
+            )
         return self
 
     def normalize_powed(self, b=2.71828):
         arr = self.rss
-        res = np.copy(arr).astype(np.float)
+        res = np.copy(arr).astype(float)
 
         zero_mask = np.logical_or(res > 50, res < -95)
         one_mask = res >= 0
-
         rest = ~np.logical_or(zero_mask, one_mask)
 
         res[zero_mask] = 0
@@ -182,7 +162,6 @@ class BaseDataProvider:
         res[rest] = ((95 + res[rest]) / 95.0) ** b
 
         self.x = res
-
         return self
 
     def generate_validation_indices(self):
@@ -191,11 +170,14 @@ class BaseDataProvider:
             return self
 
         for f_idx in range(self.num_folds):
+            existing_val = self.split_indices[f_idx].get('val', [])
+            if existing_val is not None and len(existing_val) > 0:
+                continue
 
             train_val = self.split_indices[f_idx]['train']
             train, val = train_test_split(
-                train_val, test_size=r, random_state=self.seed)
-
+                train_val, test_size=r, random_state=self.seed
+            )
             self.split_indices[f_idx]['train'] = train
             self.split_indices[f_idx]['val'] = val
 
@@ -208,21 +190,24 @@ class BaseDataProvider:
         return np.shape(self.y)[1]
 
     def generate_split_indices(self, n_splits=5, overwrite=False):
-
-        # split indices might be given by dataset (do not overwrite except if explicitly required)
         if self.dc.split_indices is not None and not overwrite:
             return self
 
-        kf = KFold(n_splits=n_splits, shuffle=True,
-                   random_state=self.seed)
+        kf = KFold(
+            n_splits=n_splits,
+            shuffle=True,
+            random_state=self.seed
+        )
         splits = []
         for s in list(kf.split(self.rss)):
-            splits += [{"train": s[0], "val": [], "test": s[1]}]
+            splits += [{
+                "train": s[0],
+                "val": [],
+                "test": s[1]
+            }]
 
         self.split_indices = splits
-
         self.num_folds = kf.get_n_splits(self.rss)
-
         return self
 
     def store(self, filename):
@@ -236,5 +221,4 @@ class BaseDataProvider:
                 dp = pickle.load(f)
         except FileNotFoundError:
             return None
-
         return dp
